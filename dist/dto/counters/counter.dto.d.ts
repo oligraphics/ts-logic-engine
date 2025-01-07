c6bf99eed7c908c55cd633b5cb14380a0c195a92
Oligraphics <11171071+oligraphics@users.noreply.github.com>
2025-01-07 23:55:55 +0100

    fix: counter value is computable

--- dist/dto/counters/counter.dto.d.ts
+++ dist/dto/counters/counter.dto.d.ts
@@ -1,6 +1,7 @@
 import { CounterTriggerDto } from '../triggers/counter.trigger.dto';
+import { Computable } from 'ts-logic-framework';
 export type CounterDto = {
-    value: number;
+    value: Computable<number>;
     triggers?: CounterTriggerDto[];
 };
 //# sourceMappingURL=counter.dto.d.ts.map
